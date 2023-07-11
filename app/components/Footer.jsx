02833f6c44149b7fdcd77258f34bbf8ca80997c7
patrgon <pgonzalezc4@outlook.es>
2023-07-11 15:54:33 -0400

    More changes to styles

--- app/components/Footer.jsx
+++ app/components/Footer.jsx
@@ -2,7 +2,7 @@ import Link from "next/link";
 
 export default function Footer() {
     return (
-        <footer className="w-full h-40 mt-auto flex flex-col p-4 border-t-2 bg-indigo-950 items-center">
+        <footer className="w-full h-40 mt-auto flex flex-col p-4 border-t-2 bg-zinc-900 items-center">
             <Link href={'/about'} className="text-cyan-600 font-bold hover:text-cyan-900">Sobre la pagina</Link>
             <Link href={'/contact'} className="text-cyan-600 font-bold hover:text-cyan-900">Contacto</Link>
             <section>
--- app/components/Header.jsx
+++ app/components/Header.jsx
@@ -18,12 +18,12 @@ const links = [
 export default function Header() {
   const activeSegment = useSelectedLayoutSegment()
   return (
-    <header className="p-4 border-b border-indigo-400 mb-6 w-full flex items-center justify-center">
+    <header className="bg-zinc-900 p-4 border-b border-indigo-400 mb-6 w-full flex items-center justify-center">
       <nav>
         <ul className="flex gap-4 list-none">
           {links.map(({ label, route, targetSegment }) => {
             return (
-              <li key={label} className={activeSegment === targetSegment ? 'text-indigo-600 text-lg' : 'text-black text-lg'}>
+              <li key={label} className={activeSegment === targetSegment ? 'text-indigo-600 text-lg font-bold' : 'text-white text-lg font-bold'}>
                 <Link href={route}  >{label}</Link>
               </li>
             );
